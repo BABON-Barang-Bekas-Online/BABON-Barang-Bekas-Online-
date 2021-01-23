e8ab6709877d1a9e94a01c93d792618f03e5ae63
BABON-Barang-Bekas-Online <permataapriliana02@gmail.com>
2021-01-24 00:14:38 +0700

    Babon

---
+++ application/config/rajaongkir.php
@@ -0,0 +1,17 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * RajaOngkir API Key
+ * Silakan daftar akun di RajaOngkir.com untuk mendapatkan API Key
+ * http://rajaongkir.com/akun/daftar(tiopanji)
+ */
+$config['rajaongkir_api_key'] = "d28960f3fdb72676451194fe7e7ea85d";
+
+/**
+ * RajaOngkir account type: starter or basic
+ * http://rajaongkir.com/dokumentasi#akun-ringkasan
+ * 
+ */
+$config['rajaongkir_account_type'] = "starter";
